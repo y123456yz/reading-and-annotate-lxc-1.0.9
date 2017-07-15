9fc2949da9ca6815086bf5449c3651dc337255cb
y123456yz <3058078489@qq.com>
2017-07-15 12:07:56 +0800

    lxc analize

---
+++ lxc-1.0.9/yyz.c
@@ -0,0 +1,10 @@
+/*
+/proc/cgroups 列出了系统中cgroup的subsystem
+/proc/XXX/cgroup  列出进程所属的cgroup
+http://www.cnblogs.com/zhengah/p/4936962.html
+
+mount -t cgroup -o cpu cpu /sys/fs/cgroup/cpu
+ 
+cgroup数hierarchy理解:  https://yq.aliyun.com/articles/64965
+
+*/
