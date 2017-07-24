f1466894afefff607217b636b448d524042dba25
y123456yz <3058078489@qq.com>
2017-07-24 22:29:59 +0800

    add libfuse-2.9.2

--- libfuse-fuse-3.1.0/test/wrong_command.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-	fprintf(stderr, "\x1B[31m\e[1m"
-		"This is not the command you are looking for.\n"
-		"You probably want to run 'python3 -m pytest test/' instead"
-		"\e[0m\n");
-	return 1;
-}
